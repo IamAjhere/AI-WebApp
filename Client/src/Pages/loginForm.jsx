65fb2812c3e8758423ed42827ffa07f330ed408e
IamAjhere <Ajcreationsajmal@gmail.com>
2022-07-18 21:27:07 +0530

    added loading for sign in and sign up page
    using ringloader from react-spinners

--- Client/src/Pages/loginForm.jsx
+++ Client/src/Pages/loginForm.jsx
@@ -2,11 +2,14 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons";
-import Axios from "axios";
+//import Axios from "axios";
+import RingLoader from "react-spinners/RingLoader";
 
-function LoginForm({ setLoginstatus, loginstatus, setloggedin, loggedin }) {
+function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const [isloading, setLoading] = useState(false);
   //eye
   const [passwordShown, setPasswordShown] = useState(false);
   const togglePassword = () => {
@@ -15,48 +18,57 @@ function LoginForm({ setLoginstatus, loginstatus, setloggedin, loggedin }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setLoading(true);
   };
-  const login = () => {};
 
   return (
     <>
-      <div className="background">
-        <div className="shape" />
-        <div className="shape" />
-      </div>
-      <form className="loginform" onSubmit={handleSubmit}>
-        <h3>Sign-In</h3>
-        <label htmlFor="username">Email</label>
-        <input
-          required
-          type="text"
-          placeholder="Email"
-          id="username"
-          onChange={(e) => setUsername(e.target.value)}
+      {isloading ? (
+        <RingLoader
+          className="load"
+          size={80}
+          color={"#F78361"}
+          loading={isloading}
         />
-        <label htmlFor="password">Password</label>
-        <input
-          required
-          type={passwordShown ? "text" : "password"}
-          placeholder="Password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <FontAwesomeIcon
-          icon={passwordShown ? faEye : faEyeSlash}
-          className="eye"
-          onClick={togglePassword}
-        />
-        <button type="submit" className="button" onClick={login}>
-          Log In
-        </button>
-        {console.log(loggedin)}
-        <h5>{!loggedin ? loginstatus : ""}</h5>
+      ) : (
+        <>
+          <div className="background">
+            <div className="shape" />
+            <div className="shape" />
+          </div>
+          <form className="loginform" onSubmit={handleSubmit}>
+            <h3>Sign-In</h3>
+            <label htmlFor="username">Email</label>
+            <input
+              required
+              type="text"
+              placeholder="Email"
+              id="username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              required
+              type={passwordShown ? "text" : "password"}
+              placeholder="Password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <FontAwesomeIcon
+              icon={passwordShown ? faEye : faEyeSlash}
+              className="eye"
+              onClick={togglePassword}
+            />
+            <button type="submit" className="button">
+              Log In
+            </button>
 
-        <Link to="../register" className="reg" replace>
-          Sign-Up
-        </Link>
-      </form>
+            <Link to="../register" className="reg" replace>
+              Sign-Up
+            </Link>
+          </form>
+        </>
+      )}
     </>
   );
 }
--- Client/src/Pages/registerForm.jsx
+++ Client/src/Pages/registerForm.jsx
@@ -2,6 +2,8 @@ import { Link, Navigate } from "react-router-dom";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import RingLoader from "react-spinners/RingLoader";
+
 import Axios from "axios";
 function RegisterForm({ loggedin }) {
   const [name, setName] = useState("");
@@ -9,8 +11,10 @@ function RegisterForm({ loggedin }) {
   const [email, setEmail] = useState("");
   const [password, setpswd] = useState("");
 
+  const [isloading, setLoading] = useState(false);
   const handleSubmit = (event) => {
     event.preventDefault();
+    setLoading(true);
   };
 
   //send data to server
@@ -22,73 +26,81 @@ function RegisterForm({ loggedin }) {
   const togglePassword = () => {
     setPasswordShown(!passwordShown);
   };
-  if (loggedin) {
-    return <Navigate to="/profile" replace={true} />;
-  }
   return (
     <>
-      <div className="background">
-        <div className="shape regc1" />
-        <div className="shape regc2" />
-      </div>
-      <form className="registerForm" onSubmit={handleSubmit}>
-        <h3>Sign-Up</h3>
-
-        <label htmlFor="username">Name</label>
-        <input
-          required
-          type="text"
-          placeholder="Name"
-          id="name"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
+      {isloading ? (
+        <RingLoader
+          className="load"
+          size={80}
+          color={"#F78361"}
+          loading={isloading}
         />
-        <label>Birthday:</label>
-        <input
-          required
-          type="date"
-          id="birthday"
-          onChange={(event) => {
-            setAge(event.target.value);
-          }}
-        ></input>
-        <label htmlFor="username">Email</label>
-        <input
-          required
-          type="text"
-          placeholder="Email"
-          id="username"
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-        <label htmlFor="password">Password</label>
+      ) : (
+        <>
+          <div className="background">
+            <div className="shape regc1" />
+            <div className="shape regc2" />
+          </div>
+          <form className="registerForm" onSubmit={handleSubmit}>
+            <h3>Sign-Up</h3>
 
-        <div>
-          <input
-            required
-            type={passwordShown ? "text" : "password"}
-            placeholder="Password"
-            id="password"
-            onChange={(event) => {
-              setpswd(event.target.value);
-            }}
-          />
-          <FontAwesomeIcon
-            icon={passwordShown ? faEye : faEyeSlash}
-            className="eye"
-            onClick={togglePassword}
-          />
-        </div>
+            <label htmlFor="username">Name</label>
+            <input
+              required
+              type="text"
+              placeholder="Name"
+              id="name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
+            />
+            <label>Birthday:</label>
+            <input
+              required
+              type="date"
+              id="birthday"
+              onChange={(event) => {
+                setAge(event.target.value);
+              }}
+            ></input>
+            <label htmlFor="username">Email</label>
+            <input
+              required
+              type="text"
+              placeholder="Email"
+              id="username"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+            <label htmlFor="password">Password</label>
 
-        <button type="submit" className="button" onClick={adduser}>
-          Log In
-        </button>
-        <Link to="../login" className="reg" replace>
-          Sign-In
-        </Link>
-      </form>
+            <div>
+              <input
+                required
+                type={passwordShown ? "text" : "password"}
+                placeholder="Password"
+                id="password"
+                onChange={(event) => {
+                  setpswd(event.target.value);
+                }}
+              />
+              <FontAwesomeIcon
+                icon={passwordShown ? faEye : faEyeSlash}
+                className="eye"
+                onClick={togglePassword}
+              />
+            </div>
+
+            <button type="submit" className="button" onClick={adduser}>
+              Log In
+            </button>
+            <Link to="../login" className="reg" replace>
+              Sign-In
+            </Link>
+          </form>
+        </>
+      )}
     </>
   );
 }
